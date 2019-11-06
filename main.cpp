db4de990b0b3404f06a3d5a5675b7f90db56c275
Sebastien Prouff <sprouff@ac-bordeaux.fr>
2019-11-06 12:12:47 +0100

    MBED 5 TEMPLATE SNIR

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/**
+ * @file main.cpp
+ * @brief Basic template file for SNIR students
+ * @author sepro
+ * @version 1.0
+ * @date 21/10/2019
+ */
+#include "mbed.h"
+#include "ThisThread.h"
+#define SLEEP_TIME                  500 // (msec)
+DigitalOut led1(LED3);
+int main()
+{
+    while (true) {
+        printf("LO SNIR \r\n");
+        led1 = !led1;
+        ThisThread::sleep_for(SLEEP_TIME);
+    }
+}
