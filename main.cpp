9ae218964e2dfdd620a2fa4e46908bdb5e24dbd6
leo.carricart <gaiden33240@gmail.com>
2020-12-17 16:54:06 +0100

    add main.cpp

---
+++ main.cpp
@@ -0,0 +1,44 @@
+/* *
+ * @file main.cpp
+ * @brief Basic Mbed 6 template file for SNIR students
+ * @author sepro
+ * @version 1.0
+ * @date 04/11/2020
+ */
+
+#include "mbed.h"
+#include "info.h"
+#include "mesure.h"
+#include "ventilation.h"
+
+#define SLEEP_TIME 200ms // (msec)
+
+int main() {
+
+float ppm; 
+float temp; 
+
+ while (true)
+  {
+    ppm = mesurerCO2();
+    temp = mesurerTemp();
+    if(ppm > 700.0 && temp > 21.0){
+        OpenWin();
+        alerteLED();
+        afficherLCD(temp,ppm);
+    }
+    else if(temp < 21.0 || ppm < 700.0){
+        CloseWin();
+        alerteLED_reset();
+        afficherLCD(temp,ppm);
+    }
+
+
+    else{
+        afficherLCD(temp,ppm);
+        alerteLED_reset();
+    }
+    alerteLED_reset();
+  }
+}
+  
