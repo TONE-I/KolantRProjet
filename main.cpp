3215921bafc26cd1964e75a0155368ccf0bad6a5
ton.testa <ton.testa@i-ac-bordeaux.fr>
2021-03-19 14:38:43 +0100

    template de Prog de calcul de temps

--- main.cpp
+++ main.cpp
@@ -13,33 +13,6 @@ DigitalOut Sortie2 = D1;
 
 //pour 130 km/h et empattement de 3 metres, laisser la boucle pour la distance inter vehicule pour les oscillos
 int main() {
-int b = 1;
-while(b == 1){
 
-  Sortie1 = 1;
-  Sortie2 = 0;
-
-  wait_us(2700);
-  Sortie1 = 0;
-  wait_us((27700-2700));
-  Sortie2 = 1;
-  wait_us(2700);
-  Sortie2 = 0;
-  //empatement du vehicule = 3 metres
-  wait_us(41500);
-  wait_us(41500);
-  Sortie1 = 1;
-  wait_us(2700);
-  Sortie1 = 0;
-  wait_us((27700-2700));
-  Sortie2 = 1;
-  wait_us(2700);
-  Sortie2 = 0;
-  wait_us(41500);
-  wait_us(41500);
-  wait_us(41500);
-  wait_us(41500);
-  
-}
 }
   
